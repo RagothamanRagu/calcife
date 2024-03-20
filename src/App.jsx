3263efacfaca1fe0a0bae8e58ae43ce233ed286e
Rago2002forever1 <you@example.com>
2024-03-20 16:48:00 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Calcicomp from './calculator/calcicom'
+// import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>calci</h1>
+        <Calcicomp/>
+       </div>
+    </>
+  )
+}
+
+export default App
